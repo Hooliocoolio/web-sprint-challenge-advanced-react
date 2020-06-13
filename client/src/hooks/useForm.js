604bac122b313479a29fc64c50bdb763105839ef
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-06-12 18:05:21 -0700

    plugged in the useform hook into the checkout form and works

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,16 +1,17 @@
 // write your custom hook here to control your checkout form
-import React, { useForm } from 'react';
+import React, { useState } from 'react';
+ 
  
     const useForm = ( initialValue ) => {
         const [ showSuccessMessage, setShowSuccessMessage ] = useState( false );
         const [ values, setValues ] = useState( initialValue );
-        const [ order, setOrder ] = useState({});
+        const [ order, setOrder ] = useState ({});
 
         const handleChanges = (e) => {
             setValues({ ...values, [e.target.name]: e.target.value });
         }
 
-        const handdleSubmit = (e) => {
+        const handleSubmit = (e) => {
             e.preventDefault();
             setShowSuccessMessage(true);
             setOrder(values)
