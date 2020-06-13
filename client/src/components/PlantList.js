81eb2334a3999c0b477a908a2358cb5948358ae2
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-06-12 17:36:47 -0700

    extracted data from json server file

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,31 +2,28 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+  // add state with a property called "plants" - initialize as an empty array
   constructor(props) {
     super(props);
     this.state = {
       plants: [],
     };
   }
-
-
-  componentDidMount(){
-    axios
-      .get('http://localhost:3333/plants')
-      .then(res => {
-        console.log(res.data);
-        this.setState({
-          plants: this.state.plants
-        })
-      })
-      .catch()
-  }
-  // add state with a property called "plants" - initialize as an empty array
-
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
-
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants')
+    .then(res => {
+      console.log(res.data.plantsData)
+      this.setState({
+        plants: res.data.plantsData
+      })
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
