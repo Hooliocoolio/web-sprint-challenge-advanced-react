841fa948b81ae0748b31d352c505d347c3f98296
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-06-14 17:16:30 -0700

    completed tests

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByText}= render(<CheckoutForm/>)
+    const header = getByText(/checkout form/i)
+    expect(header).toBeInTheDocument()
+    
+});
 
 test("form shows success message on submit with form details", () => {
+    const { getByLabelText, getByTestId, getByText } = render(<CheckoutForm />)
 
-    const [container, getByText, getByTestId] = (<CheckOutForm />)
-    const testVars
+    fireEvent.change(getByLabelText(/first name/i), { target: { value: "Hoolio" } })
+    fireEvent.change(getByLabelText(/last name/i), { target: { value: "Osuna" } })
+    fireEvent.change(getByLabelText(/address/i), { target: { value: "6969 Love Me Street" } })
+    fireEvent.change(getByLabelText(/city/i), { target: { value: "San Fran" } })
+    fireEvent.change(getByLabelText(/state/i), { target: { value: "Ca" } })
+    fireEvent.change(getByLabelText(/zip/i), { target: { value: "90210" } })
+    
 });
+ 
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,7 +1,5 @@
 // write your custom hook here to control your checkout form
 import React, { useState } from 'react';
- 
- 
     const useForm = ( initialValue ) => {
         const [ showSuccessMessage, setShowSuccessMessage ] = useState( false );
         const [ values, setValues ] = useState( initialValue );
